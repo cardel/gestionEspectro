fe797d3713a58a916c70132807a2d0e0d62bd784
Carlos Delgado <carlos@localhost.localdomain>
2012-07-22 20:23:07 -0500

    php

--- php/encabezado.php
+++ php/encabezado.php
@@ -6,7 +6,7 @@
 	$head = $solucionHead->head; 
 
 	echo "<button class=\"botonazul\">Ver PDF</button>\n";
-	echo "<input type=button class=\"botonamarillo\" onClick=\"window.open(\"http://www.google.com\", \"_black\", 'width=300,height=400' '); return false;\">Descargar XML</input>";
+	echo "<input type=button class=\"botonamarillo\" onClick=\"window.open(\"http://www.google.com\", \"_black\", 'width=300,height=400' '); return false;\" value=\"Descargar XML\"/>";
 	echo "<button class=\"botonverde\">Almacenar XML</button>";
 	echo "<p class='estilo'>Información</p>\n";
 	echo "<table width='100%' class='tabla' border='1'>\n";
