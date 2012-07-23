b6208d7d96d834345f7e58e0b50e9ee44474fbba
Carlos Delgado <carlos@localhost.localdomain>
2012-07-22 23:05:46 -0500

    php

--- php/almacenarArchivo.php
+++ php/almacenarArchivo.php
@@ -3,6 +3,7 @@
 	$nombreArchivo = $_POST['nombreArchivo'];
 	$userID = $_POST['userID'];
 	$destino = "/var/www/html/site/gestionEspectro/salidas/".$userID."/".$nombreArchivo;
+	exec("echo cp ".$file." ".$destino." >> tmp.txt ");
 	exec("cp ".$file." ".$destino);
 	
 ?>
