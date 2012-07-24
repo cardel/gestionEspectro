95f9d4a8790a12078e50cee0e459bfb9429ceaec
Carlos Delgado <carlos@localhost.localdomain>
2012-07-24 17:54:33 -0500

    Correcion XML

--- php/encabezado.php
+++ php/encabezado.php
@@ -73,9 +73,8 @@
 	echo "</tr>\n";
 	echo "<tr>\n";
 	echo "<td class='estilo'>Considerar tope de la banda</td>\n";
-	$tipoA = $head->considerTop;
 	$considerTop;
-	if($tipoA == 'true') $considerTop="Sí";
+	if(!strcmp("\"".$head->considerTop."\"" == "true")) $considerTop="Sí";
 	else  $considerTop="No";
 	echo "<td class='estilo'>$considerTop</td>\n";
 	echo "</tr>\n";
