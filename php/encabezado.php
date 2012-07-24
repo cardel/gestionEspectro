8b447bc9ee7d4d5872a35e2fe832615441cb0567
Carlos Delgado <carlos@localhost.localdomain>
2012-07-24 16:48:16 -0500

    App

--- php/encabezado.php
+++ php/encabezado.php
@@ -74,7 +74,7 @@
 	echo "<tr>\n";
 	echo "<td class='estilo'>Considerar tope de la banda</td>\n";
 	$considerTop = $head->considerTop;
-	if($considerTop != "YES") $considerTop="Sí";
+	if($considerTop \= "YES") $considerTop="Sí";
 	echo "<td class='estilo'>$considerTop</td>\n";
 	echo "</tr>\n";
 	echo "<tr>\n";
