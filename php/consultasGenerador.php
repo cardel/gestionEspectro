d4029ea6ac1b62028cedd63f41e31bdce16f536b
Carlos Delgado <carlos@localhost.localdomain>
2012-07-31 00:41:34 -0500

    Fin x hoty

--- php/consultasGenerador.php
+++ php/consultasGenerador.php
@@ -158,7 +158,7 @@
 							   while ($row =  pg_fetch_array ($result))
 							   {
 								  print ("<option value=$row[ID_Operator]>");
-								  print ("$row[ID_Operator]");
+								  print ("$row[operators_name]");
 								  print ("</option>\n");		
 								}
 								echo "</select>";	
