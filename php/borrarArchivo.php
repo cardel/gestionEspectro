b376cab39419e9de4f7d3716e4e3f609c818702b
cardel <cardel87@gmail.com>
2012-09-03 23:47:42 -0500

    update

--- php/borrarArchivo.php
+++ php/borrarArchivo.php
@@ -4,7 +4,7 @@
 	$lugar = $_POST['lugar'];
 	exec("rm -f ".$archivo);	
 	echo "<script>alert(\"Archivo borrado correctamente\");</script>";
-	if($lugar==1) echo "<script>window.locationf=\"http://http://avispa.univalle.edu.co/site/?q=node/47\"/script>";
+	if($lugar==1) echo "<script>window.locationf=\"http://http://avispa.univalle.edu.co/site/?q=node/47\"/;<script>";
 ?>
 
 
