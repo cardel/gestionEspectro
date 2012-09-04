de5308242aa1d7d2ffe4f9c86c7741efb2fe52ab
Carlos <cardel87@gmail.com>
2012-09-04 15:34:15 -0500

    Update

--- php/resultadosConsultas.php
+++ php/resultadosConsultas.php
@@ -1,4 +1,9 @@
 <?php
+
+$objconexionBD = new conexionBD();
+$objconexionBD->abrirConexion();
+
+
 $accion = $_POST["accion"];
 
 if($accion=="operador")
@@ -7,16 +12,56 @@ if($accion=="operador")
 	$departamento = $_POST["selectDepartaments"];
 	$municipio = $_POST["selectCities"];
 	$operador = $_POST["operadorSel"];
+	
     echo "<table width='100%' id='tabla1' class='tabla' border='1'>\n";		
 	echo "<thead>\n";
 	echo "<tr>\n";
-	echo "<th class='estilo'>Archivo</th>\n";
-	echo "<th class='estilo'>Visualizar</th>\n";
-	echo "<th class='estilo'>Acción</th>\n";
+	echo "<th class='estilo'>Banda de frecuencia</th>\n";
+	echo "<th class='estilo'>Rango de frecuencia</th>\n";
+	echo "<th class='estilo'>Canales asignados</th>\n";
 	echo "</tr>\n";
 	echo "</thead>\n";
 	echo "<tbody>\n";
+	
+	$tipoAsignacion = "";
+	$lugar="";
+	
+	if($divisionTerritorial==-1)
+	{
+		$tipoAsignacion = "channel_assignations_national";
+		$lugar="";
+	}
+	else
+	{			
+		if($departamento==-1)
+		{
+			$tipoAsignacion = "channel_assignations_per_territorialdivision";
+			$lugar="territorial_divisions";
+		}
+		else
+		{
+			if($municipio==-1)
+			{
+				$tipoAsignacion = "channel_assignations_per_departament";.
+				$lugar="departaments";
+			}
+			else
+			{
+				$tipoAsignacion = "channel_assignations_per_city";
+				$lugar="cities";
+			}
+		}		
+	}
+	
 	echo "<tr><td class='estilo'>1</td><td class='estilo'>2</td><td class='estilo'>3</td></tr>";
+	
+	$query = "";
+	/*
+	 * select * from channels_assignations natural join channel_assignations_national where "ID_Operator"=10;
+	  select * from channels_assignations natural join channel_assignations_per_territorialdivision natural join territorial_divisions where "ID_Operator"=10 and "ID_Territorial_Division"=2;
+
+	 */
+	
 	echo "</tbody>\n";
     echo "</table>\n";	
 	
@@ -25,10 +70,13 @@ if($accion=="operador")
 	echo $municipio."<br/>";
 	echo $operador."<br/>";
 	
+	//Activar jtables
 	echo "<script>	$('#tabla1').dataTable( {
             \"oLanguage\": {
                 \"sUrl\": \"js/spanish.txt\"
             }
         } );</script>";
 }
+
+$objconexionBD->cerrarConexion();
 ?>
