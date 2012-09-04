b8a4df8cccce957972219dcd245272eaff4f73d8
Carlos <cardel87@gmail.com>
2012-09-04 15:04:07 -0500

    Update

--- php/resultadosConsultas.php
+++ php/resultadosConsultas.php
@@ -8,6 +8,17 @@ if($accion=="operador")
 	$municipio = $_POST["selectCities"];
 	$operador = $_POST["operadorSel"];
 		
+	echo "<thead>\n";
+	echo "<tr>\n";
+	echo "<th class='estilo'>Archivo</th>\n";
+	echo "<th class='estilo'>Visualizar</th>\n";
+	echo "<th class='estilo'>Acción</th>\n";
+	echo "</tr>\n";
+	echo "</thead>\n";
+	echo "<tbody>\n";
+	echo "</tbody>\n";
+    echo "</table>\n";	
+	
 	echo $divisionTerritorial."<br/>";
 	echo $departamento."<br/>";
 	echo $municipio."<br/>";
