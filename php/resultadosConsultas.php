f0741bdaca70903a1b23ee3ef3af12d46e69b1b4
Carlos <cardel87@gmail.com>
2012-09-07 09:17:03 -0500

    Update

--- php/resultadosConsultas.php
+++ php/resultadosConsultas.php
@@ -87,7 +87,43 @@ if($accion=="operador")
         \"oLanguage\": {
                 \"sUrl\": \"js/spanish.txt\"
             }
-        } );</script>";
+        } );
+        
+          $(\"#tabla1\").css({ 
+			  a.paginate_button,
+				a.paginate_active {
+					display: inline-block;
+					background-color: #608995;
+					padding: 2px 6px;
+					margin-left: 2px;
+					cursor: pointer;
+					*cursor: hand;
+				}
+				 
+				a.paginate_active {
+					background-color: transparent;
+					border: 1px solid black;
+				}
+				 
+				a.paginate_button_disabled {
+					color: #3d6672;
+				}
+				.paging_full_numbers a:active {
+					outline: none
+				}
+				.paging_full_numbers a:hover {
+					text-decoration: none;
+				}
+				 
+				div.dataTables_paginate span>a {
+					width: 15px;
+					text-align: center;
+				}
+				 
+				div.dataTables_info {
+					padding: 9px 6px 6px 6px;
+				}});
+        </script>";
      pg_free_result($result);
 }
 
@@ -411,7 +447,9 @@ if($accion=="frecuencia")
          \"oLanguage\": {
                 \"sUrl\": \"js/spanish.txt\"
             }
-        } );</script>";
+        } );
+        
+        </script>";
 	//pg_free_result($result);
 }
 
