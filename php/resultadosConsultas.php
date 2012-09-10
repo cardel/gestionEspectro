c2744577154c4d9b80bac202c13317054b9c92e4
Carlos <cardel87@gmail.com>
2012-09-10 16:05:37 -0500

    Update

--- php/resultadosConsultas.php
+++ php/resultadosConsultas.php
@@ -446,7 +446,7 @@ if($accion=="operadores")
 	{
 	  echo "<tr>";
 	  echo "<td>".$row["operators_name"]."</td>";
-	  echo "<td><a href=\"node/49.php?idOperador=".$row["ID_Operator"]."\">Editar</a></td>";
+	  echo "<td><a href=\"?q=node/41?idOperador=".$row["ID_Operator"]."\">Editar</a></td>";
 	  echo "<td>Editar Servicios</td>";
 	  echo "</tr>";	
 	}
