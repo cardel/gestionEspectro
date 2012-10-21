6b7f7df4c115add928099e7b385ed87bfa1a715b
cardel <cardel87@gmail.com>
2012-10-20 21:59:14 -0500

    update

--- php/insertarSolucionEnBD.php
+++ php/insertarSolucionEnBD.php
@@ -33,6 +33,9 @@ foreach($soluciones as $sol)
 		$report = $sol->report;
 
 		echo "<p class='estilo'>Ingresar información en BD</p>\n";
+		/*
+		 * Asignacion es nacional
+		 */
 		if($tipoGeografico==0)
 		{
 			echo "<p style='font-size: 12pt;' >La asignación es a nivel nacional</p>\n";
@@ -113,13 +116,15 @@ foreach($soluciones as $sol)
 			}
 			echo "<p style='font-size: 12pt;' >Operación completa</p>\n";			
 		}	
-
+		/*
+		 * Asignacion es territorial
+		 */
 		if($tipoGeografico==1)
 		{
 			//$idGeografico
 			echo "<p style='font-size: 12pt;' >La asignación es a nivel territorial</p>\n";
 			
-			$query="select id_channels_assignations, id_channels_assignations_per_territorialdivision from channel_assignations_per_territorialdivision where id_channels_assignations in (select id_channels_assignations from channels_assignations where  \"ID_channels\" in (select \"ID_channels\" from channels where \"ID_frequency_ranks\"=".$rangoDeFrecuencia."));";
+			$query="select id_channels_assignations, id_channels_assignations_per_territorialdivision from channel_assignations_per_territorialdivision where id_channels_assignations in (select id_channels_assignations from channels_assignations where  \"ID_channels\" in (select \"ID_channels\" from channels where \"ID_frequency_ranks\"=".$rangoDeFrecuencia.")) and \"ID_Territorial_Division\"=".$idGeografico.";";
 			
 			echo $query;
 			
@@ -235,12 +240,128 @@ foreach($soluciones as $sol)
 				
 		}
 		
-
+		/*
+		 * Asignacion es departamental
+		 */
 		if($tipoGeografico==2)
 		{
 			//$idGeografico
 			echo "<p style='font-size: 12pt;' >La asignación es a nivel departamental</p>\n";
+			
+			
+		$query="select id_channels_assignations, id_channels_assignations_per_territorialdivision from channel_assignations_per_territorialdivision where id_channels_assignations in (select id_channels_assignations from channels_assignations where  \"ID_channels\" in (select \"ID_channels\" from channels where \"ID_frequency_ranks\"=".$rangoDeFrecuencia."));";
+			
+			echo $query;
+			
+			echo "<p style='font-size: 12pt;' >Borrando asignación actual  ... OK</p>\n";
+			$result= $objconexionBD->enviarConsulta($query);
+			
+			while ($row =  pg_fetch_array ($result))
+			{
+				//Borrar de tabla channel_assignations_per_territorialdivision 
+				$query1 = "DELETE FROM channel_assignations_per_territorialdivision WHERE id_channels_assignations=".$row['id_channels_assignations'].";";
+				$objconexionBD->enviarConsulta($query1);		
 				
+				$query2 = "DELETE FROM channels_assignations WHERE id_channels_assignations=".$row['id_channels_assignations'].";";
+				$objconexionBD->enviarConsulta($query2);						
+			}
+			
+			//Obtener maximo ID
+			
+			$maximoID;
+			$query="select max(id_channels_assignations) as max from channels_assignations;";
+			$result= $objconexionBD->enviarConsulta($query);
+			
+			while ($row =  pg_fetch_array ($result))
+			{
+				$maximoID=$row['max'];					
+			}
+			pg_free_result($result);	
+			//Nuevo ID
+			$maximoID++;
+			
+			//Crear nueva asignación
+			echo "<p style='font-size: 12pt;' >Creando nueva asignación ... OK</p>\n";
+			
+			
+			//Insertar en tablas territoriales
+			foreach($report->operator as $operator)
+			{
+				$idOperador = $operator->attributes()->name;
+				
+				//Averiguar id channel	
+				$idChannel = 0;			
+				$numeroCanal = 0;
+				$query="select min(\"ID_channels\") as min from channels where \"ID_frequency_ranks\"=".$rangoDeFrecuencia.";";
+				$result= $objconexionBD->enviarConsulta($query);
+				
+				while ($row =  pg_fetch_array ($result))
+				{
+					$idChannel=$row['min'];					
+				}	
+				pg_free_result($result);				
+				$channels = $operator->channels;
+				
+				foreach($channels->channel as $channel) 
+				{
+					$numeroCanal++;					
+					
+					if($channel==1)
+					{		
+										
+						//Aqui toca verificar si la asignación es nacional y avisar como un warning
+						
+						//Buscar si el canal está asignado
+						$query = "select id_channels_assignations from channels_assignations where \"ID_channels\" =".$idChannel.";";
+						$result= $objconexionBD->enviarConsulta($query);
+					
+						$idAsignado=-1;
+						$idAsignado =  pg_fetch_array ($result);
+
+						pg_free_result($result);
+						
+						$encontro=-1;
+						
+						//Si el canal esta asignado busquelo
+						foreach($idAsignado as $encuentraID)
+						{
+							$query = "select id_channels_assignations from channel_assignations_national where id_channels_assignations=".$encuentraID.";";
+							$result= $objconexionBD->enviarConsulta($query);
+							while ($row =  pg_fetch_array ($result))
+							{
+								$encontro=$row['id_channels_assignations'];					
+							}					
+							pg_free_result($result);								
+							
+						}
+						//Si no encuentra asigne, de otra forma diga que no se puede
+						if($encontro==-1)
+						{							
+							//Insertar en asignaciones generales
+							$query1= "insert into channels_assignations (id_channels_assignations, \"ID_Operator\", \"ID_channels\") values (".$maximoID.",".$idOperador.",".$idChannel.");";
+							echo "<br/>".$query1."<br/>";
+							$objconexionBD->enviarConsulta($query1);		
+							
+							//Insertar en asignaciones nacionales
+							$query2= "insert into channel_assignations_per_territorialdivision (id_channels_assignations, \"ID_Territorial_Division\") values (".$maximoID.",".$idGeografico.");";
+							$objconexionBD->enviarConsulta($query2);	
+							echo $query2."<br/>";
+							//Aumentar ID
+							$maximoID++;	
+						}
+						else
+						{
+							echo "<p>Alerta: El canal ".$numeroCanal." pertenece a una asignación nacional</p>";
+						}			
+											
+
+					}	
+					$idChannel++;
+
+				}
+				
+			}
+			echo "<p style='font-size: 12pt;' >Operación completa</p>\n";						
 		}
 		
 		if($tipoGeografico==3)
