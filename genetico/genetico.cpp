8780bcf8954dd785ecd0567deb7351828f3a0a45
Carlos Andrés Delgado <cardel@avispa2.(none)>
2012-10-30 10:28:46 -0500

    Update

--- genetico/genetico.cpp
+++ genetico/genetico.cpp
@@ -196,7 +196,7 @@ vector<int> calcularCostos(string in)
 //Costos
 int calcularInfraccionesARestricciones(string in)
 {
-	printf("%s", "Infracciones");
+	printf("%s", in);
 	//Maximo un operador por canal
 	int InfraccionesAnumeroOperadoresPorCanal = 0;
 	
