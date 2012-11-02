e71bdb24c53bc876cba00d17952491377c681c06
Carlos Andrés Delgado <cardel@avispa2.(none)>
2012-11-02 16:59:36 -0500

    Update

--- php/infoEntrada.php
+++ php/infoEntrada.php
@@ -10,7 +10,7 @@
 	$DisabledChannels;
 	$ChannelAssignation;
 	$numeroDeCanales;	
-	
+
 	foreach($dict->entry as $en)
 	{
 		$name = $en->attributes()->key;
@@ -20,19 +20,67 @@
 
 		if($name=="DisabledChannels")$DisabledChannels = $en->list;
 		if($name=="ChannelAssignation")$ChannelAssignation = $en->tuple;
-		if($name=="NumberChannels")$numeroDeCanales = $en->i;
+		if($name=="NumberChannels")$numeroDeCanales = $en->i;	
+		
 		
 	}
 		
-	echo "<br/>";
-	print_r($ReservedChannels);
-	echo "<br/>";
-	print_r($DisabledChannels);
-	echo "<br/>";
-	print_r($ChannelAssignInDivisions);
-	echo "<br/>";
-	echo $numeroDeCanales;
-	echo "<br/>";
+	
+	echo "<p class='estilo'>Información</p>\n";
+	
+	echo "<table width='100%' class='tabla' border='1'>\n";
+	
+	echo "<thead>\n";
+	echo "<tr>\n";
+	echo "<th class='estilo'>Concepto</th>\n";
+	echo "<th class='estilo'>Valor</th>\n";	
+	echo "</tr>\n";
+	echo "</thead>\n";
+	echo "<tbody>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Tipo de asignación geográfica</td>\n";
+	echo "<td class='estilo'>Tipo de asignación geográfica</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Lugar de asignación geográfica</td>\n";
+	echo "<td class='estilo'>Lugar de asignación geográfica</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Banda de frecuencia</td>\n";
+	echo "<td class='estilo'>Banda de frecuencia</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Número de canales de la banda</td>\n";
+	echo "<td class='estilo'>".$numeroDeCanales."</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Separación minima requerida</td>\n";
+	echo "<td class='estilo'>Separación minima requerida</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Tope de canales en la banda</td>\n";
+	echo "<td class='estilo'>Tope de canales en la banda</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Operadores presentes en la banda</td>\n";
+	echo "<td class='estilo'>Operadores presentes en la banda</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Operadores con requerimientos</td>\n";
+	echo "<td class='estilo'>Operadores con requerimientos</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Requerimientos</td>\n";
+	echo "<td class='estilo'>Requerimientos</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td class='estilo'>Maxima asignación en subdivisiones</td>\n";
+	echo "<td class='estilo'>Maxima asignación en subdivisiones</td>\n";
+	echo "</tr>\n";
+	echo "</tbody>\n";				
+	echo "</table>\n";	
+	echo "</div>";		
+	echo "<p class='estilo'>Estado actual de la banda</p>\n";
 	
 	echo '<div style="overflow:auto; width: 1200px; height :300px; align:center;">';
 	echo "ok<br/>";
@@ -65,10 +113,10 @@
 	//Deshabilitados	
 	echo "<tr>\n";
 	echo "<td class='estilo'>Canales deshabilitados</td>\n";
-	foreach($DisabledChannels as $channel)
+	foreach($DisabledChannels->i as $channel)
 	{
 		
-		if($channel->i==1) echo "<td class='estilo'>X</td>\n";
+		if($channel==1) echo "<td class='estilo'>X</td>\n";
 		else echo "<td class='estilo'></td>\n";	
 		
 	}
@@ -76,14 +124,17 @@
 	//Asignaciones en subdivisiones
 	echo "<tr>\n";
 	echo "<td class='estilo'>Asignados en subdivisiones</td>\n";
-	foreach($ChannelAssignInDivisions as $channel)
+	foreach($ChannelAssignInDivisions->i as $channel)
 	{
 		
-		if($channel->i==1) echo "<td class='estilo'>X</td>\n";
+		if($channel==1) echo "<td class='estilo'>X</td>\n";
 		else echo "<td class='estilo'></td>\n";	
 		
 	}	
 	echo "</tr>\n";
+	
+	//Divisiones locales
+	//foreach(
 
 	echo "</tbody>\n";				
 	echo "</table>\n";	
